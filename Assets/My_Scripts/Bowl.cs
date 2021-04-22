457b8132192911f4a189718f11838831035fa196
DESKTOP-R8SU23J\MAA <z44440000z@gmail.com>
2021-04-22 15:25:55 +0800

    1.完成失敗判定 2.完成視線追蹤

--- Assets/My_Scripts/Bowl.cs
+++ Assets/My_Scripts/Bowl.cs
@@ -14,47 +14,70 @@ public class Bowl : MonoBehaviour
     private Interactable interactable;
     public Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.TurnOnKinematic);
     private SteamVR_Skeleton_Poser poser;
+    private DialogueSystemTrigger dst;
     private Hand _hand;
     private void Awake()
     {
         interactable = this.GetComponent<Interactable>();
         poser = GetComponent<SteamVR_Skeleton_Poser>();
+        dst = GetComponent<DialogueSystemTrigger>();
     }
     private void Update()
     {
-
         if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
         { lastHovering = interactable.isHovering; }
+
+        if (QuestLog.GetQuestState("不要移開") == QuestState.Active)
+        {
+            if (isHold)
+            { }
+            else
+            {
+                QuestLog.SetQuestState("不要移開", QuestState.Failure);
+            }
+        }
     }
     private void HandHoverUpdate(Hand hand)
     {
+        dst.OnUse();
         _hand = hand;
         GrabTypes startingGrabType = hand.GetGrabStarting();
         bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
 
         if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
         {
-
+            isHold = true;
+            DialogueLua.SetVariable("IsHold", true);
         }
-        else if (isHold && isGrabEnding)
+        else if (isGrabEnding)
         {
-
+            isHold = false;
         }
     }
 
     private void OnHandHoverBegin(Hand hand)
     {
         hand.skeleton.BlendToPoser(poser, 0.1f);
-        DialogueLua.SetVariable("IsHold", true);
-        Debug.Log(DialogueLua.GetVariable("IsHold").AsBool);
-        isHold = true;
     }
 
     private void OnHandHoverEnd(Hand hand)
     {
         hand.skeleton.BlendToSkeleton(0.1f);
-        DialogueLua.SetVariable("IsHold", true);
-        Debug.Log(DialogueLua.GetVariable("IsHold").AsBool);
-        isHold = false;
+        if (isHold)
+        {
+            isHold = false;
+            DialogueLua.SetVariable("IsHold", true);
+        }
+    }
+
+    // IEnumerator TIMER()
+    // {
+    //     yield return new WaitForSeconds(0.2f);
+    //     isHold = false;
+    // }
+
+    void GameOver()
+    {
+
     }
 }
