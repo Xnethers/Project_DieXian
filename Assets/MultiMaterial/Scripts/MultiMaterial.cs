e1fb281aaac149f9d4b4865ae6ca51c721477408
DESKTOP-R8SU23J\MAA <z44440000z@gmail.com>
2021-05-02 18:20:25 +0800

    1.調整大小 2.嘗試使用UDIM(失敗) 3.為場景物件加上Collider

---
+++ Assets/MultiMaterial/Scripts/MultiMaterial.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UnityLabs
+{
+    [RequireComponent(typeof(Renderer))]
+    public class MultiMaterial : MonoBehaviour
+    {
+        [SerializeField]
+        MultiMaterialData m_MultiMaterialData;
+
+        MaterialArray m_MaterialArray;
+
+        public MultiMaterialData multiMaterialData
+        {
+            get { return m_MultiMaterialData; }
+        }
+
+        public MaterialArray materialArray
+        {
+            get { return m_MultiMaterialData != null ? m_MultiMaterialData.materialArrayData : m_MaterialArray; }
+
+            set { m_MaterialArray = value; }
+
+        }
+
+#if UNITY_EDITOR
+        public const string multiMaterialDataPub = "m_MultiMaterialData";
+        public const string materialArrayPub = "m_MaterialArray";
+#endif
+    }
+}
